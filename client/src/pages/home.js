5500eb49cecaf197786a7e07045875d7f37eca07
jack <biffisbuffm@gmail.com>
2020-11-01 19:45:45 -0600

    start of single page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 
-import { Grid, Transition } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import PostCard from '../components/PostCard';
 import { AuthContext } from '../context/auth';
 import PostForm from '../components/PostForm';
@@ -38,13 +38,11 @@ function Home() {
                 )}
 
                 {(
-                    <Transition.Group>
-                    {post && post.map(post => (
+                    post && post.map(post => (
                         <Grid.Column key={post.id} style={{ marginBottom: 10 }}>
                             <PostCard post={post} />
                         </Grid.Column>
-                    ))} 
-                    </Transition.Group>
+                    )) 
                 )} 
              </Grid.Row>
         </Grid>
