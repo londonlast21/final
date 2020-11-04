eaf8acb56f7ea0a9839f49aa1dac82fbe8b1bc8b
jack <biffisbuffm@gmail.com>
2020-11-04 02:18:55 -0600

    lh

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.get('*', (req, res) => {
 
 
 mongoose.connect(
-    process.env.MONGODB_URI || '0.0.0.0', 
+    process.env.MONGODB_URI || 'mongodb://localhost/transapp', 
     { useNewUrlParser: true, 
      useUnifiedTopology: true,
      useCreateIndex: true,
